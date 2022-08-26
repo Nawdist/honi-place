b87e7ae1d8d9fc0a9c9ada8db5c64b20f4af1a4b
nawdist <receptivegamer@gmail.com>
2022-08-26 10:18:38 +0100

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+import ImageHolder from "./lib/ImageHolder.svelte";
+import { appWindow } from '@tauri-apps/api/window'
+  
+</script>
+
+<main class="container">
+  <div data-tauri-drag-region class="titlebar">
+    <div on:click={appWindow.minimize} class="titlebar-button" id="titlebar-minimize">
+      <img
+        src="https://api.iconify.design/mdi:window-minimize.svg"
+        alt="minimize"
+      />
+    </div>
+    <div on:click={appWindow.toggleMaximize} class="titlebar-button" id="titlebar-maximize">
+      <img
+        src="https://api.iconify.design/mdi:window-maximize.svg"
+        alt="maximize"
+      />
+    </div>
+    <div on:click={appWindow.close} class="titlebar-button" id="titlebar-close">
+      <img src="https://api.iconify.design/mdi:close.svg" alt="close" />
+    </div>
+  </div>
+  <ImageHolder/>
+</main>
+
+<style>
+  
+</style>
